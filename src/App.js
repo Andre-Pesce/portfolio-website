42b1a31887b30339fabc1ac5a042bf92d94b5e45
Andre Pesce <andrepesce95@hotmail.com>
2020-12-03 02:05:28 +1030

    Files setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route path='/' exact />
+          </Switch>
+        </Router>
         <h1>Hi, I'm Andre Pesce!</h1>
       </header>
     </div>
