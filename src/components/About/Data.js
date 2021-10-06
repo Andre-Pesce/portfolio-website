b57bf1fa458466e59ddfcc69a5db0b7b3f5c9e97
Andre <andrepesce95@hotmail.com>
2021-10-06 13:06:08 +1030

    Updated resume and about

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -5,7 +5,7 @@ export const aboutData = {
     lightTextDesc: false,
     topLine: 'About Me',
     headline: 'This is the Headline about me',
-    description: 'I\'m a graduate software developer with attention to detail. I\'m very interested in all kinds of technology and am eager to gain more experience in software development, web design and interface design. \nI enjoy working on projects that I feel passionate about and learning new frameworks.\n\nWith recently completing my Bachelor of Information Technology in Software Development at The University of South Australia, I\'m ready to kickstart my career!',
+    description: 'I\'m a junior software developer with attention to detail. I\'m very interested in all kinds of technology and am eager to gain more experience in software development, web design and interface design. \nI enjoy working on projects that I feel passionate about and learning new frameworks.\n\nWith recently completing my Bachelor of Information Technology in Software Development at The University of South Australia, and experience working at Lockheed Martin Australia under the graduate program, I\'m motivated to further my career!',
     buttonLabel: 'Get started',
     imgStart: false,
     isVid: false,
