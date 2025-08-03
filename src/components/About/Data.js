713894ebccb0f829056517d083ef227c7d5af668
Andre Pesce <andrepesce95@hotmail.com>
2025-08-03 16:05:26 +0930

    Final new update to resume, changed about desc

--- src/components/About/Data.js
+++ src/components/About/Data.js
@@ -5,7 +5,7 @@ export const aboutData = {
     lightTextDesc: false,
     topLine: 'About Me',
     headline: 'This is the Headline about me',
-    description: 'I\'m a software engineer with great attention to detail. I\'m super interested in all kinds of technologies and am very eager to gain more experience in full stack development, web/app development, interface design, cloud services, machine learning and expanding my software engineering skills.\n\nI enjoy working on projects that I feel passionate about and learning new frameworks or programming languages. I strive to quickly become an extremely capable and well rounded software engineer so I can have the ability to create something impactful in the world.\n\nSince completing a Bachelor of Information Technology in Software Development at The University of South Australia, I have been working at Lockheed Martin Australia for over a year. I\'m always motivated to further my career and take on challenges that allow me to grow!',
+    description: 'I\'m an experienced software engineer with 4+ years in defence and R&D sectors, specialising in full-stack development, DevOps, Systems Administration, and complex system integrations. I\'m super interested in all kinds of technologies and am very eager to gain more experience in full stack development, web/app development, interface design, cloud services, machine learning and expanding my software engineering skills.\n\n I enjoy working on projects that I feel passionate about and learning new frameworks or programming languages. I\'m always motivated to further my career and take on challenges that allow me to grow!',
     buttonLabel: 'Get started',
     imgStart: false,
     isVid: false,
